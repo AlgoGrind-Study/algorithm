3d45e6dcf1d9f0e951319cf2ef048807a548342e
장재영 <oasis1340@gmail.com>
2025-06-25 00:03:01 +0900

    Update 장재영.py

--- 1주차/영수증/장재영.py
+++ 1주차/영수증/장재영.py
@@ -1,1 +1,11 @@
+total = int(input())
+count = int(input())
 
+for i in range(0, count):
+  money, num = map(int, input().split())
+  total -= money * num
+
+if total == 0:
+  print('Yes')
+else:
+  print('No')
