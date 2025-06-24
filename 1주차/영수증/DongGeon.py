175555dd364967ab6d8ad7b1a0f07b533db101de
KimDongGeon <Dcode0971@gmail.com>
2025-06-24 23:56:41 +0900

    Create DongGeon.py

---
+++ 1주차/영수증/DongGeon.py
@@ -0,0 +1,14 @@
+total = int(input())
+num = int(input())
+result = 0
+
+for i in range(num):
+    product = input().split()
+    price = int(product[0])
+    stock = int(product[1])
+    result += int(price) * int(stock)
+
+if result == total:
+    print("YES")
+else:
+    print("NO")
