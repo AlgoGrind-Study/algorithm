e70bf735f019e2bd210a10b9b5048c00cfca5c83
KimDongGeon <Dcode0971@gmail.com>
2025-07-10 23:10:51 +0900

    Update DongGeon.py

--- 3주차/행렬 덧셈/DongGeon.py
+++ 3주차/행렬 덧셈/DongGeon.py
@@ -1,1 +1,19 @@
+# 2738번 행렬 덧셈
+n, m = map(int, input().split())
 
+# 리스트 생성
+a = []
+b = []
+
+for _ in range(n):
+    nums = list(map(int, input().split()))
+    a.append(nums)
+
+for _ in range(n):
+    nums = list(map(int, input().split()))
+    b.append(nums)
+
+for i in range(n):
+    for j in range(m):
+        print(a[i][j] + b[i][j], end=' ')
+    print()
