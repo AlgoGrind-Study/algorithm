493107cf137239b75e344376f2420cb7f8e8db8c
신정훈 <junghoon.shin3377@gmail.com>
2025-07-14 12:01:12 +0900

    Create 정훈.java

---
+++ 3주차/행렬 덧셈/정훈.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();  
+        int B = sc.nextInt(); 
+
+        int[][] arr1 = new int[A][B];
+        int[][] arr2 = new int[A][B];
+        
+        for (int i = 0; i < A; i++) {
+            for (int j = 0; j < B; j++) {
+                arr1[i][j] = sc.nextInt();
+            }
+        }
+  
+        for (int i = 0; i < A; i++) {
+            for (int j = 0; j < B; j++) {
+                arr2[i][j] = sc.nextInt();
+                arr1[i][j] += arr2[i][j];
+            }
+        }
+
+        for (int i = 0; i < A; i++) {
+            for (int j = 0; j < B; j++) {
+                System.out.print(arr1[i][j] + " ");
+            }
+            System.out.println(); 
+        }
+    }
+}
