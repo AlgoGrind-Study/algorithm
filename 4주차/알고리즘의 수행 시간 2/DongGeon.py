8b576c71629cfa487e7edff287dd64c3f4bd2c31
KimDongGeon <Dcode0971@gmail.com>
2025-07-17 23:57:42 +0900

    Create DongGeon.py

---
+++ 4주차/알고리즘의 수행 시간 2/DongGeon.py
@@ -0,0 +1,13 @@
+# 24263 알고리즘 수업 - 알고리즘의 수행 시간 2
+n = int(input())
+# MenOfPassion(A[], n) {
+#     sum = 0;
+#     for i in range(n):
+#         sum = sum + A[i] # 코드1
+#     return sum;
+# }
+
+# 수행횟수
+print(n)
+# 최고차항
+print(1)
