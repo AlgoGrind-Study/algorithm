3ffc3752516f06c196114915b1df4630ef49df77
jiihyun12 <j22h.h0h@gmail.com>
2025-08-03 20:38:28 +0900

    Create jiihyun12.py

---
+++ 6주차/도비의 영어 공부/jiihyun12.py
@@ -0,0 +1,11 @@
+while True:
+    n = input()
+
+    if n == '#' :
+        break
+
+    target = n[0].lower()
+    sentence = n[2:].lower()
+    count = sentence.count(target)
+
+    print(n[0], count)
