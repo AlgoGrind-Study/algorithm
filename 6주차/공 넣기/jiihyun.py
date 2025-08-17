2b45d7bcf626be54522d99f765bcaf9f98dabb4e
jiihyun12 <j22h.h0h@gmail.com>
2025-08-17 14:57:46 +0900

    Create jiihyun.py

---
+++ 6주차/공 넣기/jiihyun.py
@@ -0,0 +1,10 @@
+
+N, M = map(int, input().split())
+baskets = [0] * N
+
+for _ in range(M):
+    i, j, k = map(int, input().split())
+    baskets[i - 1 : j] = [k] * (j - i + 1)
+
+print(*baskets)
+
